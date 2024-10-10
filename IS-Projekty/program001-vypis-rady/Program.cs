d2efedd616a6bdbfa76e1d9d01cb07be7d4d4329
Vojtěch Devátý <vojtech.devaty@tul.cz>
2024-10-10 13:32:07 +0200

    #1 zakladni program s moznosti opakovani

---
+++ IS-Projekty/program001-vypis-rady/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program { 
+
+static void Main(){
+
+    //chci, aby se program opakoval po stisku "a"
+    string again =  "a";
+    while(again == "a"){
+        Console.Clear();
+        Console.WriteLine("************************");
+        Console.WriteLine("******Výpis řady********");
+        Console.WriteLine("************************");
+        Console.WriteLine("*****Vojtěch Devátý*****");
+        Console.WriteLine("************************\n\n");
+        Console.WriteLine();
+
+
+        // Vstup od uživatele - TO-DO
+
+
+        //Logika pro výpis řady - TO-DO
+
+        //Opakování programu
+        Console.WriteLine("Pro opakování programu stisktňete hodnotu a");
+        again = Console.ReadLine();
+ }
+
+
+
+}
+
+}

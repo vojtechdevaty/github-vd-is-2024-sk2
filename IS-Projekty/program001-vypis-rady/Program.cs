c02bbb10a10f3dcd81052f958fa6b2f71c396622
vojtechdevaty <vojtech.devaty@tul.cz>
2024-10-17 09:46:18 +0200

    Update Program.cs

--- IS-Projekty/program001-vypis-rady/Program.cs
+++ IS-Projekty/program001-vypis-rady/Program.cs
@@ -1,33 +1,66 @@
 ﻿using System;
+using System.Collections.Specialized;
 
-class Program { 
+class Program {
 
-static void Main(){
+    static void Main() {
 
-    //chci, aby se program opakoval po stisku "a"
-    string again =  "a";
-    while(again == "a"){
-        Console.Clear();
-        Console.WriteLine("************************");
-        Console.WriteLine("******Výpis řady********");
-        Console.WriteLine("************************");
-        Console.WriteLine("*****Vojtěch Devátý*****");
-        Console.WriteLine("************************\n\n");
-        Console.WriteLine();
+        // chci, aby se program opakoval po stisku klávesy "a"
+        string again = "a";
+        while(again == "a") {
+            //Console.Clear();
+            Console.WriteLine("**************************");
+            Console.WriteLine("******* Výpis řady *******");
+            Console.WriteLine("**************************");
+            Console.WriteLine("****** Vojtěch Devátý *******");
+            Console.WriteLine("**************************\n\n");
+            Console.WriteLine();
+
+            // Vstup od uživatele - špatná varianta
+            //Console.Write("Zadejte první číslo řady: ");
+            //int first = int.Parse(Console.ReadLine());
+
+            // Vstup od uživatele - lepší varianta
+            Console.Write("Zadejte první číslo řady (celé číslo): ");
+            int first;
+            while(!int.TryParse(Console.ReadLine(), out first)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu první číslo řady (celé číslo): ");
+            }
+
+            Console.Write("Zadejte poslední číslo řady (celé číslo): ");
+            int last;
+            while(!int.TryParse(Console.ReadLine(), out last)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu poslední číslo řady (celé číslo): ");
+            }
+
+            Console.Write("Zadejte diference (celé číslo): ");
+            int step;
+            while(!int.TryParse(Console.ReadLine(), out step)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte diference (celé číslo): ");
+            }
+
+            // Výpis uživatelského vstupu
+            Console.WriteLine();
+            Console.WriteLine("======================================");
+            Console.WriteLine("První číslo řady: {0}", first);
+            Console.WriteLine("Poslední číslo řady: {0}", last);
+            Console.WriteLine("Diference: {0}", step);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // Logika pro výpis řady - TO-DO
+            int current = first;
+            while(current <= last) {
+                Console.WriteLine(current);
+                current = current + step;
+            }
 
 
-        // Vstup od uživatele - TO-DO
+            // Opakování programu
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
+    }   
 
-
-        //Logika pro výpis řady - TO-DO
-
-        //Opakování programu
-        Console.WriteLine("Pro opakování programu stisktňete hodnotu a");
-        again = Console.ReadLine();
- }
-
-
-
-}
 
 }

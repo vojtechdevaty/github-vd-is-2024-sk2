4f7f89a555397a7d39e468a99db4011be5805caa
vojtechdevaty <vojtech.devaty@tul.cz>
2024-10-23 20:23:56 +0200

    Update Program.cs

--- program002b-soucet-cifer/Program.cs
+++ program002b-soucet-cifer/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string again = "a";
+        while (again == "a")
+        {
+            Console.Clear();
+            Console.WriteLine("*******************************");
+            Console.WriteLine("**** Součet a součin cifer ****");
+            Console.WriteLine("******** Vojtěch Devátý *********");
+            Console.WriteLine("*******************************");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.Write("Zadejte číslo (celé číslo): ");
+            string input = Console.ReadLine();
+            int number;
+            while (!int.TryParse(input, out number))
+            {
+                Console.Write("Nezadali jste celé číslo. Zadejte první číslo znovu: ");
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Zadané hodnoty: ");
+            Console.WriteLine("číslo: {0}", number);
+            Console.WriteLine("========================================================");
+
+            string numberString = Math.Abs(number).ToString();
+            int sum = 0; 
+
+            foreach (char digitChar in numberString)
+            {
+                
+                int digitValue = digitChar - '0';
+                sum += digitValue;
+                Console.WriteLine("Aktuální zbytek = {0}", digitChar);
+            }
+
+            Console.WriteLine("\n\nSoučet cifer čísla {0} je {1}", number, sum);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Program můžete opakovat stiskem klávesy 'a'");
+            again = Console.ReadLine();
+        }
+    }
+}

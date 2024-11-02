7be8c8d61cee10affca30010d37640a7fcae0da2
Vojtěch Devátý <vojtech.devaty@tul.cz>
2024-11-02 13:57:54 +0100

    #ukol003 + presub do slozky

---
+++ ukoly/ukol003/Program.cs
@@ -0,0 +1,75 @@
+﻿       using System;
+using System.Collections.Specialized;
+
+class Program {
+
+    static void Main() {
+
+       
+       
+        string again = "a";
+        while(again == "a") {
+            //Console.Clear();
+            Console.WriteLine("******************************");
+            Console.WriteLine("******* ukol003 *************");
+            Console.WriteLine("******************************");
+            Console.WriteLine("******** Vojtěch Devátý*******");
+            Console.WriteLine("******************************\n\n");
+            Console.WriteLine();
+
+            // Vstup od uživatele - lepší varianta
+            Console.Write("Zadejte šířku obrazce (celé číslo): ");
+            int width;
+            while(!int.TryParse(Console.ReadLine(), out width)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu šířku obrazce (celé číslo): ");
+            }
+
+            Console.Write("Zadejte výšku obrazce (celé číslo): ");
+            int height;
+            while(!int.TryParse(Console.ReadLine(), out height)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu výšku obrazce (celé číslo): ");
+            }
+            
+
+            Console.WriteLine();
+            
+            
+            
+            
+            for (int i = 0; i <= height/2; i++) {
+            for (int j = 0; j <= width; j++) {
+                if (j > width / 2) {
+                    Console.Write("*");
+                } else {
+                    Console.Write(" "); 
+                }
+            }
+            Console.WriteLine(); 
+        }
+            
+                    for (int i = 0; i <= height/2; i++) {
+            for (int j = 0; j <= width; j++) {
+                if (j > width / 2) {
+                    Console.Write("");
+                } else {
+                    Console.Write("*"); 
+                }
+            }
+            Console.WriteLine(); 
+        }
+            
+            
+            
+            
+            
+            
+
+            
+             // Opakování programu
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
+    }   
+
+
+}
--- ukoly/ukol003/obj/Debug/net8.0/ukol003.AssemblyInfo.cs
+++ ukoly/ukol003/obj/Debug/net8.0/ukol003.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ukol002")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ukol003")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+589911ba5a0129b3c7e89adad93e46aa07584e05")]
-[assembly: System.Reflection.AssemblyProductAttribute("ukol002")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ukol002")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+328cd684a33d3fbb37e64d4d368d4f2ad2ab5ce1")]
+[assembly: System.Reflection.AssemblyProductAttribute("ukol003")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ukol003")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Vygenerované třídou WriteCodeFragment nástroje MSBuild
